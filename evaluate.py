5c2a1c7373c62be931eaa5f4da035c4fdca63333
efti-dot <eftih31@gmail.com>
2026-06-18 19:59:02 +0600

    handle api key runtime error

--- evaluate.py
+++ evaluate.py
@@ -6,7 +6,9 @@ load_dotenv()
 API_KEY = os.getenv("GEMINI_API_KEY")
 
 def run_evaluation():
-    print("Evaluating...")
+    #print("Evaluating...")
+    if not API_KEY:
+        raise RuntimeError("Gemini api key not found. Please set it in a .env file.")
 
 
 
