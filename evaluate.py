87109a5a0594f1ef2f905d555878ee4ebdd5ed63
efti-dot <eftih31@gmail.com>
2026-06-18 20:03:02 +0600

    load a demo json file(for testing)

--- evaluate.py
+++ evaluate.py
@@ -1,14 +1,21 @@
 from dotenv import load_dotenv
 import os
+import json
 
 load_dotenv()
 
 API_KEY = os.getenv("GEMINI_API_KEY")
+SCENARIOS_PATH = "scenarios.json"
 
 def run_evaluation():
     #print("Evaluating...")
     if not API_KEY:
         raise RuntimeError("Gemini api key not found. Please set it in a .env file.")
+    
+    with open(SCENARIOS_PATH, "r", encoding="utf-8") as f:
+        scenarios = json.load(f)
+
+    result = []
 
 
 
